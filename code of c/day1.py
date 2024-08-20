1a082782501806bb7f9e30e368db90ef1e85e6b2
MOHAMMED FAIZAN MAQSOODAHMED PATEL <68345709+FAIZAN101013@users.noreply.github.com>
2024-08-20 07:50:43 -0700

    Add files via upload

---
+++ code of c/day1.py
@@ -0,0 +1,6 @@
+print("How are you Doing , Welcome the Band Name Generator")
+city = str(input("Where Did you grew up(CITY)? : \n")).upper()
+pet = str(input("What was the Name of your first pet? : \n")).upper()
+print(f"Your Brand Name could be {city} {pet} !!!!!")
+print()
+
